308be541efc4a606d140df3bd978ee3b0405e4ce
Geoffroy PLANQUART (Dev VM) <geoffroy@planquart.fr>
2011-12-12 01:47:46 +0100

    Resolved shift/reduced errors. Decomposed help in multiple methods

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -41,31 +41,58 @@ void Parser::help(int command) const
 		switch(command)
 		{
 			case 0:
-				std::cout << "Global help" << std::endl;
-				std::cout << "\t\"target\"\tConfigure targets. " << HELP_SEE("target") << std::endl;
-				std::cout << "\t\"config\"\tSpecify global configuration options. " << HELP_SEE("config") << std::endl;
-				std::cout << "\t\"exit\"\tExit configuration and build makefile." << std::endl;
-				std::cout << std::endl << "Most commands support `show' (or `list') to show or list specific informations." << std::endl;
+				this->help_state_chooser();
 				break;
 			case T_TARGET:
-				std::cout << "Manage targets" << std::endl;
-				std::cout << "\t`target add'  Add a new target." << std::endl;
-				std::cout << "\t`target edit' Update an existing target." << std::endl;
-				std::cout << "\t`target rm'   Remove a target." << std::endl;
-				std::cout << "\t`target list' List all targets." << std::endl;
+				this->help_target();
 				break;
 			case T_CONFIG:
-				std::cout << "Specify global configuration options." << std::endl;
-				std::cout << "It is recommended to first tune these settings, because they are then copied to all new target." << std::endl;
-				std::cout << "If you edit these settings later, the changes will only be applied to new targets." << std::endl << std::endl;
-				std::cout << "\t`config set debug (true|false)'             Specify global debug value" << std::endl;
-				std::cout << "\t`config set verbose (true|false)'           Specify global debug value" << std::endl;
-				std::cout << "\t`config add (include|library) path {FILE}'  Add the specified include / library path" << std::endl;
-				std::cout << "\t`config add library {NAME}'                 Add the specified library to be linked" << std::endl;
-				std::cout << "\t`config (show|list)'                        Show configuration" << std::endl;
+				this->help_config();
 				break;
 		}
 	}
+}
+void Parser::help_state_chooser() const
+{
+	switch(this->state)
+	{
+		case INITIAL:
+			this->help_global();
+			break;
+		case TARGET:
+			this->help_target_mode();
+			break;
+	}
+}
+void Parser::help_global() const
+{
+	std::cout << "Global help" << std::endl;
+	std::cout << "\t\"target\"\tConfigure targets. " << HELP_SEE("target") << std::endl;
+	std::cout << "\t\"config\"\tSpecify global configuration options. " << HELP_SEE("config") << std::endl;
+	std::cout << "\t\"exit\"\tExit configuration and build makefile." << std::endl;
+	std::cout << std::endl << "Most commands support `show' (or `list') to show or list specific informations." << std::endl;
+}
+void Parser::help_target() const
+{
+	std::cout << "Manage targets" << std::endl;
+	std::cout << "\t`target add'  Add a new target." << std::endl;
+	std::cout << "\t`target edit' Update an existing target." << std::endl;
+	std::cout << "\t`target rm'   Remove a target." << std::endl;
+	std::cout << "\t`target list' List all targets." << std::endl;
+}
+void Parser::help_target_mode() const
+{
+}
+void Parser::help_config() const
+{
+	std::cout << "Specify global configuration options." << std::endl;
+	std::cout << "It is recommended to first tune these settings, because they are then copied to all new target." << std::endl;
+	std::cout << "If you edit these settings later, the changes will only be applied to new targets." << std::endl << std::endl;
+	std::cout << "\t`config set debug (true|false)'             Specify global debug value" << std::endl;
+	std::cout << "\t`config set verbose (true|false)'           Specify global debug value" << std::endl;
+	std::cout << "\t`config add (include|library) path {FILE}'  Add the specified include / library path" << std::endl;
+	std::cout << "\t`config add library {NAME}'                 Add the specified library to be linked" << std::endl;
+	std::cout << "\t`config (show|list)'                        Show configuration" << std::endl;
 }
 
 ParserState Parser::getState() const
