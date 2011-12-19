5984957a5abf6f8433073e3c76407c4fc81b6115
Geoffroy PLANQUART (Dev VM) <geoffroy@planquart.fr>
2011-12-19 08:07:12 +0100

    Added strtolower and strtoupper C functions

---
+++ src/GP/C/stringCase.c
@@ -0,0 +1,26 @@
+#include "strtolower.h"
+
+void strtolower(char* string)
+{
+	while (*string != 0)
+	{
+		if (*string >= 'A' && *string <= 'Z')
+		{
+			*string = *string + 'a' - 'A';
+		}
+		string++;
+	}
+}
+
+void strtoupper(char* string)
+{
+	while (*string != 0)
+	{
+		if (*string >= 'a' && *string <= 'z')
+		{
+			*string = *string + 'A' - 'a';
+		}
+		string++;
+	}
+}
+
---
+++ src/GP/C/stringCase.h
@@ -0,0 +1,9 @@
+#ifndef GP_C_STRINGCASE_H
+#define GP_C_STRINGCASE_H
+
+void strtolower(char* string);
+
+void strtoupper(char* string);
+
+#endif /* GP_C_STRINGCASE_H */
+
