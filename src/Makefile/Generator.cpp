72f8507ee59ded92dc43c366bb2b8706838a4386
Geoffroy PLANQUART <geoffroy@planquart.fr>
2012-01-18 22:19:00 +0100

    Updating Generator. Now target get a lowercased name.

--- src/Makefile/Generator.cpp
+++ src/Makefile/Generator.cpp
@@ -2,6 +2,7 @@
 
 #include "Generator.hpp"
 #include "Target.hpp"
+#include "../GP/Strings/case.h"
 
 using namespace Makefile;
 
@@ -25,20 +26,14 @@ const Generator::targets_map& Generator::getTargets() const
 }
 Target& Generator::getTarget(const std::string& name)
 {
-	try
-	{
-		return *(this->targets.at(name).get());
-	}
-	catch(std::out_of_range ex)
-	{
-		throw std::out_of_range("No such target");
-	}
+	return *(this->getTargetPointer(name).get());
 }
 Generator::target_type Generator::getTargetPointer(const std::string& name)
 {
+	std::string nameToLower = GP::Strings::stringToLower(name);
 	try
 	{
-		return this->targets.at(name);
+		return this->targets.at(nameToLower);
 	}
 	catch(std::out_of_range ex)
 	{
@@ -47,28 +42,31 @@ Generator::target_type Generator::getTargetPointer(const std::string& name)
 }
 Target& Generator::addTarget(const std::string& name)
 {
-	if (Generator::reservedWords.find(name) != Generator::reservedWords.end())
+	std::string nameToLower = GP::Strings::stringToLower(name);
+	if (Generator::reservedWords.find(nameToLower) != Generator::reservedWords.end())
 	{
 		throw std::invalid_argument(std::string("Invalid use of reserved name : ") + name);
 	}
 
-	target_type& target = this->targets[name];
+	target_type& target = this->targets[nameToLower];
 	if (target.get() != nullptr)
 	{
 		throw std::invalid_argument("Target already exists");
 	}
-	target = target_type(new Target(*this, name));
+	target = target_type(new Target(*this, nameToLower));
 
 	return *(target.get());
 }
 void Generator::removeTarget(const std::string& name)
 {
-	target_type target = this->targets[name];
+	std::string nameToLower = GP::Strings::stringToLower(name);
+
+	target_type target = this->targets[nameToLower];
 	if (target.get() == nullptr)
 	{
 		throw std::out_of_range("No such target");
 	}
-	this->targets.erase(name);
+	this->targets.erase(nameToLower);
 }
 
 Config& Generator::getConfig()
